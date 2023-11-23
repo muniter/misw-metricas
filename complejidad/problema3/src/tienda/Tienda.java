028f3347dfaced0861a0331f64675eb1448f3360
Javier López <graulopezjavier@gmail.com>
2023-11-23 11:22:00 -0500

    updates

--- complejidad/problema3/src/tienda/Tienda.java
+++ complejidad/problema3/src/tienda/Tienda.java
@@ -2,7 +2,7 @@ package tienda;
 
 
 public class Tienda {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Tienda tienda = new Tienda();
         Producto libro1 = new Producto("libro1", 20000);
         Producto libro2 = new Producto("libro2", 30000);
@@ -18,20 +18,28 @@ public class Tienda {
         tienda.imprimirRecibo(productos, cliente3);
     }
 
-    public void imprimirRecibo(Producto[] productos, Cliente cliente){
-        int total = 0;
+    public void imprimirRecibo(Producto[] productos, Cliente cliente) {
+        var total = calcularTotal(productos, cliente);
+        System.out.println("Total para " + cliente.getNombre() + ": " + total);
+
+    }
+    
+    private float calcularTotal(Producto[] productos, Cliente cliente) {
+        float total = 0;
         for (Producto producto : productos) {
             total += producto.getPrecio();
         }
+        total = this.aplicarDescuento(cliente, total);
+        return total;
+    }
 
+    private float aplicarDescuento(Cliente cliente, float total) {
         if (cliente.getTipo().equals("Premium")) {
             total *= 0.9;
-        }
-        else if (cliente.getTipo().equals("VIP")) {
+        } else if (cliente.getTipo().equals("VIP")) {
             total *= 0.8;
         }
 
-        System.out.println("Total para " + cliente.getNombre() + ": " + total);
-
+        return total;
     }
 }
